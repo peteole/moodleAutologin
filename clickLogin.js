aa915a8cb113619234c08df86cb924819c01f84f
Ole Petersen <peteole2707@gmail.com>
2022-01-29 11:36:47 +0100

    Improved login behavior

--- clickLogin.js
+++ clickLogin.js
@@ -1,3 +1,3 @@
 const linkElements=document.getElementsByTagName('a')
-const loginLink=[...linkElements].find(link=>link.innerText==='TUM LOGIN')
+const loginLink=[...linkElements].find(link=>link.innerText==='TUM LOGIN'||link.innerText==='With TUM ID')
 window.location.href=loginLink.href
